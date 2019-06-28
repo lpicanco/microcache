7fa3c91da1666506341044755c0aa81692dc8305
Luiz Picanço <lpicanco@gmail.com>
2019-06-28 17:02:19 -0300

    Adding groupcache/lru to benchmark

--- cache_benchmark_test.go
+++ cache_benchmark_test.go
@@ -3,6 +3,7 @@ package microcache
 import (
 	"sync"
 	"testing"
+	"github.com/golang/groupcache/lru"
 )
 
 func BenchmarkMapPut(b *testing.B) {
@@ -25,11 +26,22 @@ func BenchmarkSyncMapPut(b *testing.B) {
 	}
 }
 
+func BenchmarkGroupCachePut(b *testing.B) {
+	lru := lru.New(100)
+	var mu sync.RWMutex
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mu.Lock()
+		lru.Add(i, i)
+		mu.Unlock()
+	}
+}
+
 func BenchmarkPut(b *testing.B) {
-	b.StopTimer()
 	cache := NewCache(DefaultConfiguration(100))
 
-	b.StartTimer()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		cache.Put(string(i), i)
 	}
@@ -90,4 +102,42 @@ func BenchmarkPutGetConcurrent(b *testing.B) {
 			i++
 		}
 	})
+}
+
+func BenchmarkGroupCacheConcurrent(b *testing.B) {
+	cache := lru.New(10000)
+	var mu sync.RWMutex
+
+	var wg sync.WaitGroup
+	wg.Add(b.N * 2)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		go func(i int) {
+			mu.Lock()
+			cache.Add(i, i)
+			mu.Unlock()
+			wg.Done()
+		}(i)
+
+		if i%10 == 3 {
+			wg.Add(1)
+			go func(i int) {
+				mu.Lock()
+				cache.Remove(i)
+				mu.Unlock()
+				wg.Done()
+			}(i - 1)
+		}
+
+		go func(i int) {
+			mu.RLock()
+			cache.Get(i)
+			mu.RUnlock()
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
 }
